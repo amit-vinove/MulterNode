a1462a34885444162466041c714dc854507ddace
Amit Kumar <amit.kumar3@mail.vinove.com>
2022-09-14 18:16:48 +0530

    filter

--- routes/indexGrid.js
+++ routes/indexGrid.js
@@ -1,5 +1,5 @@
 var express = require("express");
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 var multer = require("multer");
 var path = require("path");
 var jwt = require("jsonwebtoken");
@@ -12,52 +12,51 @@ var router = express.Router();
 var employee = empModel.find({});
 const crypto = require("crypto");
 
-var imgName="";
+var imgName = "";
 var flag;
 
 const mongoURI = "mongodb://localhost:27017/employee";
 const conn = mongoose.createConnection(mongoURI);
 let gfs;
-conn.once("open", () => {gfs = Grid(conn.db, mongoose.mongo); gfs.collection("employee")});
+conn.once("open", () => {
+  gfs = Grid(conn.db, mongoose.mongo);
+  gfs.collection("employee");
+});
 
 router.use(express.static(__dirname + "./public/"));
 
 const storage = new GridFsStorage({
   url: mongoURI,
   file: function (req, file) {
-      return new Promise((resolve, reject) => {
-          crypto.randomBytes(16, (err, buf) => {
-              if (err) {
-                  return reject(err);
-              }
-              const fileInfo = {
-                  filename: file.originalname,
-                  bucketName: "employee",
-              };
-              resolve(fileInfo);
-          });
+    return new Promise((resolve, reject) => {
+      crypto.randomBytes(16, (err, buf) => {
+        if (err) {
+          return reject(err);
+        }
+        const fileInfo = {
+          filename: file.originalname,
+          bucketName: "employee",
+        };
+        resolve(fileInfo);
       });
-  }
-})
+    });
+  },
+});
 
 var upload = multer({
   fileFilter: async function (req, file, cb) {
     console.log(file.originalname);
     imgName = file.originalname;
-    gfs.files.find({ filename: file.originalname })
-        .toArray((err, files) => {
-          console.log(files ,"type",typeof files , files.length);
-            if (files.length > 0) 
-            {
-              req.fileValidationError = `File Already Exists`;
-                cb(null, false, req.fileValidationError);
-            }
-             else               
-              cb(null, true ,req.body.typeOfFile);
-             })
-  },storage
+    gfs.files.find({ filename: file.originalname }).toArray((err, files) => {
+      console.log(files, "type", typeof files, files.length);
+      if (files.length > 0) {
+        req.fileValidationError = `File Already Exists`;
+        cb(null, false, req.fileValidationError);
+      } else cb(null, true, req.body.typeOfFile);
+    });
+  },
+  storage,
 }).single("file");
-
 
 router.get("/", function (req, res, next) {
   employee.exec(function (err, data) {
@@ -66,13 +65,30 @@ router.get("/", function (req, res, next) {
       title: "Image Records",
       records: data,
       error: "",
-      success:""
+      success: "",
     });
   });
 });
 
-router.post("/", upload, async function (req, res, next) {
+router.get("/images/:filename", (req, res) => {
+  gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+    if (!file || file.length === 0) {
+      return res.status(404).json({
+        err: "No file exists",
+      });
+    }
+    if (file.contentType === "image/jpeg" || file.contentType === "image/png") {
+      const readstream = gfs.createReadStream(file.filename);
+      readstream.pipe(res);
+    } else {
+      res.status(404).json({
+        err: "Not an image",
+      });
+    }
+  });
+});
 
+router.post("/", upload, async function (req, res, next) {
   if (req.fileValidationError) {
     employee.exec(function (err, data) {
       if (err) throw err;
@@ -80,7 +96,7 @@ router.post("/", upload, async function (req, res, next) {
         title: "Image Records",
         records: data,
         error: "Image Already Exist",
-        success:""
+        success: "",
       });
     });
   } else {
@@ -97,11 +113,44 @@ router.post("/", upload, async function (req, res, next) {
           title: "Image Records",
           records: data,
           success: "Image Uploaded Successfully",
-          error:""
+          error: "",
         });
       });
     });
   }
+});
+
+router.post("/search/", function (req, res, next) {
+  var fltrName = req.body.fltrName;
+  var fltrEmail = req.body.fltrEmail;
+  var fltrImage = req.body.fltrImage;
+  console.log(fltrName, fltrEmail, fltrImage);
+
+  if (fltrName != "" && fltrEmail != "" && fltrImage != "") {
+    var flterParameter = { name: fltrName, email: fltrEmail, image: fltrImage };
+  } 
+  else if (fltrName != "" && fltrEmail == "" && fltrImage != "") {
+    var flterParameter = { name: fltrName, image: fltrImage };
+  }
+   else if (fltrName == "" && fltrEmail != "" && fltrImage != "") {
+    var flterParameter = {email: fltrEmail, image: fltrImage };
+  } 
+  else if (fltrName == "" && fltrEmail == "" && fltrImage != "") {
+    var flterParameter = { image: fltrImage };
+  } 
+  else {
+    var flterParameter = {};
+  }
+  var employeeFilter = empModel.find(flterParameter);
+  employeeFilter.exec(function (err, data) {
+    if (err) throw err;
+    res.render("index", {
+      title: "Image Records",
+      records: data,
+      success: "",
+      error: "",
+    });
+  });
 });
 
 module.exports = router;
